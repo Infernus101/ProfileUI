1aea72dd313f0e7f59e20a2193870a7815fba642
Infernus <Infernus101@users.noreply.github.com>
2019-06-09 18:09:33 +0530

    remove useless startup messages

--- src/Infernus101/Main.php
+++ src/Infernus101/Main.php
@@ -21,7 +21,6 @@ class Main extends PluginBase implements Listener {
 	public $flag = false;
 	
 	public function onEnable(){
-		$this->getServer()->getLogger()->notice("[ProfileUI] Enabled! - By Infernus101");
 		$file = "config.yml";
 		if(!file_exists($this->getDataFolder() . $file)){
 		@mkdir($this->getDataFolder());
@@ -45,7 +44,6 @@ class Main extends PluginBase implements Listener {
 	
 	public function onDisable(){
 		$this->saveStat();
-		$this->getServer()->getLogger()->notice("[ProfileUI] Disabled! - By Infernus101");
 	}
 	
 	public function saveStat(){
